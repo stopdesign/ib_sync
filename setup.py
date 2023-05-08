d277f7f363c33f86243d3d3ea49f1c04f3466013
Gregory <gregory@stopdesign.ru>
2023-05-08 12:46:57 -0700

    fix packages

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="ib_sync",
-    version="0.2.2",
+    version="0.2.3",
     url="https://github.com/stopdesign/ib_sync.git",
     author="IBKR and Gregory",
     description="IBKR API Client and some tools",
-    packages=["ib_sync", "ibapi", "mcal"],
+    packages=["ib_sync", "ibapi", "mcal", "mcal.*"],
     package_dir={"": "src"},
     install_requires=["pandas_market_calendars ~= 4.1, < 5"],
 )
